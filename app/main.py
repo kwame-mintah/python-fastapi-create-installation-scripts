0b3c3f944a71382edf77ab5aeffff1807361eaba
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2023-05-29 16:52:27 +0100

    feat(main): enable cors in fastapi server
    resolve issue with react app getting a cors error when making a request to fastapi server.

--- app/main.py
+++ app/main.py
@@ -1,9 +1,28 @@
+import os
+
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.routers import users
+
+frontend_origin = os.environ.get("FRONTEND_SERVICE_URL")
 
 app = FastAPI()
 app.include_router(users.router)
 
+origins = [
+    frontend_origin
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["POST", "GET"],
+    allow_headers=["*"],
+    max_age=3600,
+)
+
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000)
